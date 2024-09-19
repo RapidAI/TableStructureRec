801035b95d6f243a5d91a5d140fc0a0c7e1192f5
SWHL <liekkaskono@163.com>
2024-09-19 14:20:38 +0800

    ci(lineless_table_rec): Enable automatic CI

--- demo_lineless.py
+++ demo_lineless.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-import os
+from pathlib import Path
 
 from lineless_table_rec import LinelessTableRecognition
 from lineless_table_rec.utils_table_recover import (
@@ -10,7 +10,9 @@ from lineless_table_rec.utils_table_recover import (
     plot_rec_box_with_logic_info,
 )
 
-output_dir = "outputs"
+output_dir = Path("outputs")
+output_dir.mkdir(parents=True, exist_ok=True)
+
 img_path = "tests/test_files/lineless_table_recognition.jpg"
 table_rec = LinelessTableRecognition()
 
@@ -18,11 +20,13 @@ html, elasp, polygons, logic_points, ocr_res = table_rec(img_path)
 print(f"cost: {elasp:.5f}")
 
 complete_html = format_html(html)
-os.makedirs(os.path.dirname(f"{output_dir}/table.html"), exist_ok=True)
-with open(f"{output_dir}/table.html", "w", encoding="utf-8") as file:
+
+save_table_path = output_dir / "table.html"
+with open(save_table_path, "w", encoding="utf-8") as file:
     file.write(complete_html)
 
 plot_rec_box_with_logic_info(
     img_path, f"{output_dir}/table_rec_box.jpg", logic_points, polygons
 )
 plot_rec_box(img_path, f"{output_dir}/ocr_box.jpg", ocr_res)
+print(f"The results has been saved under {output_dir}")
