9eb825a2dd1850a0b48cb4bec1cedc60139150e4
Joker1212 <519548295@qq.com>
2024-11-12 13:14:40 +0800

    test: add test for new param

--- demo_wired.py
+++ demo_wired.py
@@ -15,17 +15,8 @@ output_dir.mkdir(parents=True, exist_ok=True)
 
 table_rec = WiredTableRecognition()
 
-img_path = "tests/test_files/wired/wired_big_box.png"
-html, elasp, polygons, logic_points, ocr_res = table_rec(
-    img_path,
-    version="v2",  # 默认使用v2线框模型，切换阿里读光模型可改为v1
-    morph_close=True,  # 是否进行形态学操作,辅助找到更多线框,默认为True
-    more_h_lines=True,  # 是否基于线框检测结果进行更多水平线检查，辅助找到更小线框, 默认为True
-    more_v_lines=True,  # 是否基于线框检测结果进行更多垂直线检查，辅助找到更小线框, 默认为True
-    extend_line=True,  # 是否基于线框检测结果进行线段延长，辅助找到更多线框, 默认为True
-    need_ocr=True,  # 是否进行OCR识别, 默认为True
-    rec_again=True,  # 是否针对未识别到文字的表格框,进行单独截取再识别,默认为True
-)
+img_path = "tests/test_files/wired/table1.png"
+html, elasp, polygons, logic_points, ocr_res = table_rec(img_path)
 
 print(f"cost: {elasp:.5f}")
 
@@ -38,6 +29,6 @@ with open(save_table_path, "w", encoding="utf-8") as file:
 plot_rec_box_with_logic_info(
     img_path, f"{output_dir}/table_rec_box.jpg", logic_points, polygons
 )
-plot_rec_box(f"{output_dir}/table_rec_box.jpg", f"{output_dir}/ocr_box.jpg", ocr_res)
+plot_rec_box(img_path, f"{output_dir}/ocr_box.jpg", ocr_res)
 
 print(f"The results has been saved under {output_dir}")
